df4ff259ece0d26e63bd42ebacc74ca11640a38e
pero4l <lilpero03@gmail.com>
2026-06-15 12:12:22 +0100

    first push

---
+++ mobile/components/themed-view.tsx
@@ -0,0 +1,18 @@
+import { View, type ViewProps } from 'react-native';
+import { cssInterop } from 'nativewind';
+
+import { useThemeColor } from '@/hooks/use-theme-color';
+
+export type ThemedViewProps = ViewProps & {
+  lightColor?: string;
+  darkColor?: string;
+};
+
+export function ThemedView({ style, lightColor, darkColor, ...otherProps }: ThemedViewProps) {
+  const backgroundColor = useThemeColor({ light: lightColor, dark: darkColor }, 'background');
+
+  return <View style={[{ backgroundColor }, style]} {...otherProps} />;
+}
+
+cssInterop(ThemedView, { className: 'style' });
+
